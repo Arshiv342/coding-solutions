588ddcd047d00aa351cd50add8f29502abe22e9c
Anuj Tiwai <arshiv342@gmail.com>
2026-08-29 09:07:38 +0530

    ✅ CS2023_GIFT - Rating 390 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-29

---
+++ codechef/easy/CS2023_GIFT---Rating-390/solution.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+	
+		    int x = sc.nextInt();
+		    int n = sc.nextInt();
+		    int m = sc.nextInt();
+		    if( x + m >= n) {
+		        System.out.println("YES");
+		        
+		    } else {
+		        System.out.println("NO");
+		    }
+             
+
+	
+  }
+}
