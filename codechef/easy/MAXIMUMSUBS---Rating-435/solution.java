a45f6564421a1fbae4ba459e42649feaa303fb7e
Anuj Tiwai <arshiv342@gmail.com>
2026-09-01 09:47:11 +0530

    ✅ MAXIMUMSUBS - Rating 435 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-01

---
+++ codechef/easy/MAXIMUMSUBS---Rating-435/solution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		for(int i = 0; i < t; i++) {
+		    int x = sc.nextInt();
+		
+		   int total = (x*60) / 30;
+		   System.out.println(total);
+		    
+		}
+
+	}
+}
