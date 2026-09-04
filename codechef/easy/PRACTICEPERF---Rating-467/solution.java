a74c1679701e4f0045f19661a814b84cef639f60
Anuj Tiwai <arshiv342@gmail.com>
2026-09-04 16:35:08 +0530

    ✅ PRACTICEPERF - Rating 467 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-04

---
+++ codechef/easy/PRACTICEPERF---Rating-467/solution.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int p1 = sc.nextInt();
+		int p2 = sc.nextInt();
+		int p3 = sc.nextInt();
+		int p4 = sc.nextInt();
+		 int count = 0;
+        
+        if (p1 >= 10) count++;;
+        if (p2 >= 10) count++;;
+        if (p3 >= 10) count++;;
+        if (p4 >= 10) count++;;
+        
+      System.out.println(count);
+	}
+}
