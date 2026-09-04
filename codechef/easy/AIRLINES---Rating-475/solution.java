7b9958f29e783ff67e7504c4193a413dce2a7f61
Anuj Tiwai <arshiv342@gmail.com>
2026-09-04 19:41:04 +0530

    ✅ AIRLINES - Rating 475 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-04

--- codechef/easy/AIRLINES---Rating-475/solution.java
+++ codechef/easy/AIRLINES---Rating-475/solution.java
@@ -8,11 +8,14 @@ class Codechef
 	{
 		Scanner sc = new Scanner(System.in);
 		int t = sc.nextInt();
-		for(int i = 0; i < t; i++) {
-		    int x = sc.nextInt();
-		    int y =sc.nextInt();
-		    int p = Math.min(x,y);
-		    System.out.println(p);
+		for(int i = 0; i < t; i++)  {
+		    int x  = sc.nextInt();
+		    int y = sc.nextInt();
+		    int z = sc.nextInt();
+		    int a = 10 * x;
+		    int b = Math.min(y, a);
+		    int c = b * z;
+		    System.out.println(c);
 		}
 
 	}
