ed7b93a310d30d281b95f1a16982dab709502347
Anuj Tiwai <arshiv342@gmail.com>
2026-09-04 22:10:18 +0530

    ✅ DOMINANT - Rating 487 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-04

---
+++ codechef/easy/DOMINANT---Rating-487/solution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        
+        while (T-- > 0) {
+            int X = sc.nextInt();
+            int Y = sc.nextInt();
+            
+            
+            if (Y <= 1.07 * X) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
+}
