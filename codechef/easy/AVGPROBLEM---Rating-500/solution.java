54800dfe085b717921c80ab7041f1b0a4bfc3c2a
Anuj Tiwai <arshiv342@gmail.com>
2026-09-06 10:31:46 +0530

    ✅ AVGPROBLEM - Rating 500 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-06

---
+++ codechef/easy/AVGPROBLEM---Rating-500/solution.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+     Scanner sc = new Scanner(System.in);
+     int t = sc.nextInt();
+     for(int i = 0; i < t; i++) {
+         int a = sc.nextInt();
+         int b = sc.nextInt();
+         int c = sc.nextInt();
+         double avg = (a + b ) / 2.0;
+         if(avg > c) {
+         System.out.println("YES");
+         
+          } else {
+              System.out.println("NO");
+          }
+     }
+	}
+}
