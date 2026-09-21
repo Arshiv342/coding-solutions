c5b4604c06619a2d629d5f4b66490fd4601ca107
Anuj Tiwai <arshiv342@gmail.com>
2026-09-21 22:23:16 +0530

    ✅ QUALIFY - Rating 594 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-21

---
+++ codechef/easy/QUALIFY---Rating-594/solution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	   System.out.println(System.in);
+	   int t = sc,nextInt();
+	   for(int i =0; i < t; i++) {
+	       int x = sc.nextInt();
+	       int a = sc.nextInt();
+	       int b = sc.nextInt();
+	       
+	       System.out.println()
+	   }
+
+	}
+}
