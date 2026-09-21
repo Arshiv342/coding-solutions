ffdb74797df0e8280e829211f39685d52f7e62ca
Anuj Tiwai <arshiv342@gmail.com>
2026-09-21 22:27:02 +0530

    ✅ QUALIFY - Rating 594 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-21

--- codechef/easy/QUALIFY---Rating-594/solution.java
+++ codechef/easy/QUALIFY---Rating-594/solution.java
@@ -7,13 +7,16 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 	   System.out.println(System.in);
-	   int t = sc,nextInt();
+	   int t = sc.nextInt();
 	   for(int i =0; i < t; i++) {
 	       int x = sc.nextInt();
 	       int a = sc.nextInt();
 	       int b = sc.nextInt();
-	       
-	       System.out.println()
+	       if(a + b*2 >= x) {
+	           System.out.println("Qualify");
+	       } else {
+	       System.out.println("NotQualify");
+	       }
 	   }
 
 	}
