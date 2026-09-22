9e155df512332719ddc03fc594090ddc6b15839e
Anuj Tiwai <arshiv342@gmail.com>
2026-09-22 11:21:37 +0530

    ✅ JENGA - Rating 613 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-22

--- codechef/easy/JENGA---Rating-613/solution.java
+++ codechef/easy/JENGA---Rating-613/solution.java
@@ -11,7 +11,7 @@ class Codechef
 		for(int i = 0; i < t; i++) {
 		    int n = sc.nextInt();
 		    int x = sc.nextInt();
-		    if(x % n == 0) {
+		    if(n <= x) {
 		        System.out.println("YES");
 		        
 		    } else {
