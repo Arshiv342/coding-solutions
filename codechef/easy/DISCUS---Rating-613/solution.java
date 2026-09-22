192d747c55df44cee297abf10b2c82d34c614a31
Anuj Tiwai <arshiv342@gmail.com>
2026-09-22 11:36:23 +0530

    ✅ DISCUS - Rating 613 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-22

---
+++ codechef/easy/DISCUS---Rating-613/solution.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		for(int i = 0; i < t; i++) {
+		    int n = sc.nextInt();
+		    if (n <= 15) {
+                if (n == 11 || n == 12 ||  n == 13 || n == 14 || n == 15) {
+                    System.out.println("Lower Single");
+                } else {
+                    System.out.println("Lower Double");
+                }
+            } else { 
+              if (n == 26 || n == 27 || n == 28 || n == 29 || n == 30)  {
+                    System.out.println("Upper Single");
+                } else {
+                    System.out.println("Upper Double");
+                }
+            }
+		}
+
+	}
+}
