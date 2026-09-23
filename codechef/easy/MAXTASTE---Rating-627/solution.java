6e17674a027b7c7d1d85323f1aae4f2804cda517
Anuj Tiwai <arshiv342@gmail.com>
2026-09-23 15:56:03 +0530

    ✅ MAXTASTE - Rating 627 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-23

---
+++ codechef/easy/MAXTASTE---Rating-627/solution.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		for(int i = 0; i < t; i++) {
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int d = sc.nextInt();
+		   int m = Math.max(
+                Math.max(a + c, a + d),
+                Math.max(b + c, b + d)
+            );
+
+            System.out.println(m); 
+		
+		}
+	}
+}
